fd1e87b68829f79e5c7f29363488b29fdd8f3ae9
Thiago Sciotta <thiagog3@gmail.com>
2016-03-28 11:25:39 -0300

    DELETE no-sense folder xD

--- Dribbble-master/js/Project/filters.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var filters =  angular.module('dribbble.filters', []);
-
-filters.filter('dabbbleDate', function ($filter) {
-	return function (datestr, format) {
-		return $filter('date')(Date.parse(datestr) || datestr, format);
-	}
-});
